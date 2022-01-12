8267c4d2ab5e34e3aa088524aea49cb991862384
andrewc2020 <andrewc2020@gmail.com>
2022-01-12 22:22:56 +0000

    added sinon stub examples

--- accounts/meme.routes.js
+++ accounts/meme.routes.js
@@ -1,4 +1,4 @@
-var Meme = require('./meme.model');
+var Meme = require('../accounts/meme.model');
  
 module.exports.allMemes = function(req, res) {
     Meme.find({ repost: req.params.reposts }, function(err, memes) {
