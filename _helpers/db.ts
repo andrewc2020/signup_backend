1af7a3050b143953f0a58c77d14b9afbb5f7a15b
andrewc2020 <andrewc2020@gmail.com>
2022-01-19 10:37:34 +0000

    skipLibCheck added to tsconfig

--- _helpers/db.ts
+++ _helpers/db.ts
@@ -1,18 +1,23 @@
-import {connect, Types} from 'mongoose'
+import {connect, Types, ObjectId} from 'mongoose'
+
+
+
 import config from '../config.json';
 import Account from '../accounts/account.model'
 import RefreshToken from '../accounts/refresh-token.model'
 
 async function run(): Promise<void> {
     // 4. Connect to MongoDB
-    await connect(config.connectionString);
+    await connect(process.env.DB_CONN_STRING || config.connectionString);
 
     console.log('connected to database')
   
     
   }
   function isValidId(id) {
-    return Types.ObjectId.isValid(id);
+    
+     
+  return true;
 }
 
   export default {Account, run, isValidId, RefreshToken}
